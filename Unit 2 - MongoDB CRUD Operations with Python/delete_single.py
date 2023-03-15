0df8c7764e03bdc5259d29d7f167805e6736138f
Princesacorderosa <59964928+Princesacorderosa@users.noreply.github.com>
2023-03-15 14:15:19 +0100

    lesson 5

---
+++ Unit 2 - MongoDB CRUD Operations with Python/delete_single.py
@@ -0,0 +1,40 @@
+
+import pprint
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+#import ObjectId class from bson package (part of PyMongo distribution) to enable querying
+from bson.objectid import ObjectId
+
+#Load config from .env file
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
+
+#Connect to MongoDB cluster with MongoClient
+client = MongoClient(MONGODB_URI)
+
+# Get reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter by ObjectId
+document_to_delete = {"_id": ObjectId("62d6e04ecab6d8e130497485")}
+
+# Search for document before delete
+print("Searching for target document before delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+# Write an expression that deletes the target account.
+result = accounts_collection.delete_one(document_to_delete)
+
+# Search for document after delete
+print("Searching for target document after delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+
+client.close()
